d8330ef3bad7a892b06c19c00ff3ebf4bec1d20a
itlaonong <itlaonong@gmail.com>
2021-01-25 18:13:22 +0800

    3-2 查询请求

---
+++ imooc-security-demo/src/main/java/com/imooc/security/browser/UserController.java
@@ -0,0 +1,37 @@
+package com.imooc.security.browser;
+
+import com.imooc.dto.User;
+import com.imooc.dto.UserQueryCondition;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public List<User> query(UserQueryCondition condition,
+                            @PageableDefault(page = 2, size = 17, sort = "username,asc") Pageable pageable) {
+
+        System.out.println(ReflectionToStringBuilder.toString(condition, ToStringStyle.MULTI_LINE_STYLE));
+
+        System.out.println(pageable.getPageSize());
+        System.out.println(pageable.getPageNumber());
+        System.out.println(pageable.getSort());
+
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        return users;
+    }
+}
