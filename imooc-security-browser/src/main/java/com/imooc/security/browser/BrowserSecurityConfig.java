d29aaa5b71a7d00337ca53e58aeadf8bff6de721
itlaonong <itlaonong@gmail.com>
2021-01-26 14:29:59 +0800

    4-2 SpringSecurity基本原理

---
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.imooc.security.browser;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+
+        //http.formLogin()
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+
+    }
+}
