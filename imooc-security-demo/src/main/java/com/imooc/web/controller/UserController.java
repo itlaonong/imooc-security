17ece78da69dbb4086446a95cf924f1b640aafc2
itlaonong <itlaonong@gmail.com>
2021-01-27 17:35:45 +0800

    4-6 认证流程源码级详解

--- imooc-security-demo/src/main/java/com/imooc/web/controller/UserController.java
+++ imooc-security-demo/src/main/java/com/imooc/web/controller/UserController.java
@@ -10,6 +10,8 @@ import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +22,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/me")
+    public Object getCurrentUser(@AuthenticationPrincipal UserDetails user) {
+        return user;
+    }
 
     @JsonView(User.UserSimpleView.class)
     @GetMapping
